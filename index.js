dc40a0b930378f42848a4b9b0abf8e0dad4095a7
1XC <62459731+1XCRLKQtMTGAQAvSxv0Yb@users.noreply.github.com>
2021-07-10 22:06:30 -0500

    yes

---
+++ index.js
@@ -0,0 +1,78 @@
+const { Plugin } = require("powercord/entities")
+const { getModule , channels } = require("powercord/webpack")
+const { createBotMessage } = getModule([ "createBotMessage" ], false)
+const { receiveMessage }   = getModule([ "receiveMessage" ], false)
+
+module.exports = class YoutubeMusic extends Plugin {
+    constructor() {
+        super()
+        
+    }
+    
+    async sendBotMessage(content, title, footer, author, url, footerURL ) {
+        const received = createBotMessage(channels.getChannelId())
+        received.author.username = "Penis"
+        // received.author.avatar = "https://bloximages.newyork1.vip.townnews.com/jewishaz.com/content/tncms/assets/v3/editorial/9/e0/9e0f4054-50aa-11e9-8169-030b16b2a5e4/5c9ba015c2cc7.image.jpg"
+        console.log(received)
+        received.embeds.push({
+            color: 0x0099ff,
+            title: title,
+            url: url,
+            author: {
+                name: author,
+            },
+            description: content,
+            timestamp: new Date(),
+            footer: {
+                text: footer,
+                icon_url: footerURL,
+            },
+        })
+        return receiveMessage(received.channel_id, received)
+    }
+
+    async startPlugin() {
+        
+        
+        const { sendBotMessage } = this
+        
+        
+        powercord.api.commands.registerCommand({
+            command: "penis",
+            description: "yes",
+            usage: "{c}",
+            executor: async (args) => {
+                sendBotMessage(`
+                ........................„„-~^^~„-„„_
+                ..................„-^*'' : : „'' : : : : *-„
+                ..............„-* : : :„„--/ : : : : : : : '\
+                ............./ : : „-* . .| : : : : : : : : '|
+                ............/ : „-* . . . | : : : : : : : : |
+                ............\„-* . . . . .| : : : : : : : :'|
+                ............/ . . . . . . '| : : : : : : : |
+                ........../ . . . . . . . .'\ : : : : : : : |
+                ......../ . . . . . . . . . .\ : : : : : : |
+                ......./ . . . . . . . . . . . '\ : : : : : /
+                ....../ . . . . . . . . . . . . . *-„„„„-*'
+                ....'/ . . . . . . . . . . . . . . '|
+                .../ . . . . . . . ./ . . . . . . .|
+                ../ . . . . . . . .'/ . . . . . . .'|
+                ./ . . . . . . . . / . . . . . . .'|
+                '/ . . . . . . . . . . . . . . . .'|
+                '| . . . . . \ . . . . . . . . . .|
+                '| . . . . . . \„_^- „ . . . . .'|
+                '| . . . . . . . . .'\ .\ ./ '/ . |
+                | .\ . . . . . . . . . \ .'' / . '|
+                | . . . . . . . . . . / .'/ . . .|
+                | . . . . . . .| . . / ./ ./ . .|`)
+            }
+        })
+
+    }
+    
+    pluginWillUnload() {
+        // Object.values(commands).forEach(command => powercord.api.commands.unregisterCommand(command.command))
+        powercord.api.commands.unregisterCommand("penis")
+        
+    }
+}
